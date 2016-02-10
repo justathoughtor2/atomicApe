a5e120b2f3ef96e1cf10f0fc9a88a9cda2b8b0b4
bananas <bananas@atomicape.xyz>
2016-02-10 01:10:49 -0500

    encagedApe
    First release to replace Atom with Brackets. This change is due to the
    fact that Atom doesn't play nicely with Cygwin when it comes to locating
    executables for its linter and other extensions. It is a separate branch
    so that users have a choice about whether or not to upgrade.

--- cygwin/home/bananas/atom/app-1.4.3/resources/app/apm/fake/spec/fake-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import FakeView from '../lib/fake-view';
-
-describe('FakeView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
--- cygwin/home/bananas/atom/bin/apm.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-"%~dp0\..\app-1.4.3\resources\app\apm\bin\apm.cmd" %*
